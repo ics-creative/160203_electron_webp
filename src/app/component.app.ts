32c1d90063a337a0c9076ba96104f10ea638f11e
ics-ikeda <ikeda@ics-web.jp>
2016-03-28 16:45:37 +0900

    ドラッグ・アンド・ドロップに対応

--- src/app/component.app.ts
+++ src/app/component.app.ts
@@ -28,6 +28,35 @@ export class AppComponent {
   private files:FileData[];
   private selectedFile:string;
 
+  constructor() {
+    // -------------------------------
+    // ドラッグ&ドロップの動作を阻止する
+    // -------------------------------
+    document.addEventListener("dragover", (event:DragEvent)=> {
+      this.handleDragOver(event);
+    });
+    document.addEventListener("drop", (event:DragEvent)=> {
+      this.handleDrop(event);
+    });
+
+  }
+
+  private handleDragOver(event:DragEvent) {
+    event.preventDefault();
+  }
+
+  private handleDrop(event:DragEvent) {
+    event.preventDefault();
+
+    const files = <FileList> event.dataTransfer.files;
+    const filenames:string[] = [];
+    for (let i = 0; i < files.length; i++) {
+      let file = <File>files[i]
+      filenames.push(file["path"]);
+    }
+    this.updateFiles(<string[]> filenames);
+  }
+
   /**
    * 読み込みするためのファイルを開く
    */
@@ -35,21 +64,21 @@ export class AppComponent {
     var win = browserWindow.getFocusedWindow();
 
     dialog.showOpenDialog(
-        win,
-        // どんなダイアログを出すかを指定するプロパティ
-        {
-          properties: ["openFile", "multiSelections"],
-          filters: [
-            {
-              name: "Image Files",
-              extensions: ["png", "gif", "jpeg", "jpg", "svg", "webp"]
-            }
-          ]
-        },
-        // [ファイル選択]ダイアログが閉じられた後のコールバック関数
-        (filenames:string[]) => {
-          this.updateFiles(filenames);
-        });
+      win,
+      // どんなダイアログを出すかを指定するプロパティ
+      {
+        properties: ["openFile", "multiSelections"],
+        filters: [
+          {
+            name: "Image Files",
+            extensions: ["png", "gif", "jpeg", "jpg", "svg", "webp"]
+          }
+        ]
+      },
+      // [ファイル選択]ダイアログが閉じられた後のコールバック関数
+      (filenames:string[]) => {
+        this.updateFiles(filenames);
+      });
 
     // バインディングのトリガー・・・
     setInterval(()=> {
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './component.app'
 bootstrap(AppComponent);
+

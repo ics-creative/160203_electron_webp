45ba0c7009090621fe3aa0b56ab83e3cc90a5422
ics-ikeda <ikeda@ics-web.jp>
2017-03-27 14:29:24 +0900

    Version up : Electron

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-"use strict";
+'use strict';
 
-const app = require('app');  // アプリケーション作成用モジュールをロード
-const BrowserWindow = require('browser-window');
+const electron = require('electron');
+const app = electron.app;  // アプリケーション作成用モジュールをロード
+const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
@@ -13,7 +14,7 @@ app.on('window-all-closed', () => {
 // アプリケーションの初期化が完了したら呼び出されます。
 app.on('ready', () => {
   // メインウィンドウを作成します。
-  mainWindow = new BrowserWindow({width: 1000, height: 700, title:"WebP画像を作る君"});
+  mainWindow = new BrowserWindow({width: 1000, height: 700, title: 'WebP画像を作る君'});
 
   // メインウィンドウに表示するURLを指定します。
   mainWindow.loadURL('file://' + __dirname + '/index.html');
